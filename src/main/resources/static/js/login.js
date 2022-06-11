d0971e9775eead75e519de3d672654735a2167ea
Illia <44270738+MachinaIlucha@users.noreply.github.com>
2022-06-11 16:18:58 +0300

    Delete src/main/resources/static/js directory

--- src/main/resources/static/js/login.js
+++
@@ -1,9 +0,0 @@
-$("#login-form").submit(function (e) {
-    e.preventDefault();
-
-    $.ajax({
-        url: "login",
-        type: "POST",
-        data: $("#login-form").serialize(),
-    });
-});
